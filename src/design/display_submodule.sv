34d5bfb3215d24c9a4763bfc31ea4be6a1e212ff
Juan Pablo Elizondo Espinoza <pablo.elizondo17@hotmail.com>
2024-09-21 16:38:54 -0600

    datos del display

---
+++ src/design/display_submodule.sv
@@ -0,0 +1,19 @@
+module display(
+    input logic [11 : 0] sum_result,
+    output logic [6 : 0] units,
+    output logic [6 : 0] tens,
+    output logic [6 : 0] hundreds,
+    output logic [6 : 0] thousands
+);
+
+    //Almacena temporalmente cada dígito
+    logic [3:0] units_digit;
+    logic [3:0] tens_digit;
+    logic [3:0] hundreds_digit;
+    logic [3:0] thousands_digit;
+
+
+
+
+
+endmodule
