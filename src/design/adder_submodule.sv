1e2fd273c436a4d2273729651faf9bbd36348ae7
Juan Pablo Elizondo Espinoza <147214546+pabloelizondo17@users.noreply.github.com>
2024-09-29 10:39:47 -0600

    Update adder_submodule.sv

--- src/design/adder_submodule.sv
+++ src/design/adder_submodule.sv
@@ -1,29 +1,26 @@
 module adder_submodule (
     input logic clk,              
     input logic reset,           
-    input logic [11:0] number1, 
-    input logic [11:0] number2,
-    input logic enable, 
-    output logic [11:0] sum_result, //Resultado de la suma
-    output logic sum_state          //Estado de la suma (finalizada o no)
+    input logic [11 : 0] number1,        // Entrada de 12 bits máximo
+    input logic [11 : 0] number2,        // Entrada de 12 bits máximo
+    input logic enable,                  // Señal de habilitación (se puede hacer la suma o no)
+    output logic [11 : 0] sum_result,    // Resultado de 12 bits máximo
+    output logic sum_state               // Estado de la suma (finalizada o no)
 );
 
-    logic [13:0] sum_reg;
-    logic sum_ready;
+    logic [11 : 0] sum_reg = 12'b0;     // Registro temporal de 12 bits máximo
+    logic sum_ready = 1'b0;             // Registro de estado temporal
 
-    always_ff @(posedge clk, posedge reset) begin
+    always_ff @(posedge clk or posedge reset) begin
         if (reset) begin
-            sum_reg <= 14'd0;
-            sum_ready <= 1'b0;
+            sum_reg <= 12'b0;          
+            sum_ready <= 1'b0;         
         end else if (enable) begin
             sum_reg <= number1 + number2;
-            sum_ready <= 1'b1;
-        end else begin
-            sum_ready <= 1'b0;
+            sum_ready <= 1'b1; 
         end
     end
 
     assign sum_result = sum_reg;
-    assign sum_state = sum_ready;
-    
+    assign sum_state = sum_ready;  
 endmodule
